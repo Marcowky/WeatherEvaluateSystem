1b84285122b0e333eb84e9ec355d5d2fdcc4dcaf
Marco <marcowky@qq.com>
2025-11-18 22:42:07 +0800

    feat: 实现了 default api client，chat_with_messages_return_text

--- src/model/client.py
+++ src/model/client.py
@@ -4,16 +4,19 @@ from util.config import load_config
 
 
 class ModelClient:
-    def __init__(self, api_type: str = "siliconflow"):
+    def __init__(self, api_type: str = None):
         self.config = self.load_api_config(api_type=api_type)
         self.client = OpenAI(
             api_key=self.config["api_key"], 
             base_url=self.config["base_url"]
         )
 
-    def load_api_config(self, api_type: str = "siliconflow") -> dict:
-        config = load_config()["llm_api"][api_type]
-        return config
+    def load_api_config(self, api_type: str = None) -> dict:
+        config = load_config()
+        if api_type is None:
+            api_type = config["llm_api"]["default"]
+        api_config = config["llm_api"][api_type]
+        return api_config
 
     def chat_with_messages(self, model: str, messages: list, **kwargs) -> dict:
         response = self.client.chat.completions.create(
@@ -22,6 +25,10 @@ class ModelClient:
             **kwargs
         )
         return response
+    
+    def chat_with_messages_return_text(self, model: str, messages: list, **kwargs) -> str:
+        response = self.chat_with_messages(model=model, messages=messages, **kwargs)
+        return response.choices[0].message.content
 
     def chat_with_prompt(self, model: str, prompt: str, **kwargs) -> dict:
         messages = [{"role": "user", "content": prompt}]
